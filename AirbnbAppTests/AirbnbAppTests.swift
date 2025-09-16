5415a9176ccfeb0cd5fbaa4d4dc6f23f786b626a
Khawla Zarrami <zarramikhaoula@gmail.com>
2025-09-16 09:03:04 +0100

    Initial Commit

---
+++ AirbnbAppTests/AirbnbAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbAppTests.swift
+//  AirbnbAppTests
+//
+//  Created by Khawla Zarrami on 16/09/2025.
+//
+
+import Testing
+@testable import AirbnbApp
+
+struct AirbnbAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
