48fd900761ba32ec2b841f50fae84edb2d37c142
Khawla Zarrami <zarramikhaoula@gmail.com>
2025-09-23 10:29:49 +0100

    Implement destination search view in progress

--- AirbnbApp/Core/Explore/ExploreView.swift
+++ AirbnbApp/Core/Explore/ExploreView.swift
@@ -8,27 +8,40 @@
 import SwiftUI
 
 struct ExploreView: View {
+    @State private var showDestinationSearchView = false
+    
+    
     var body: some View {
         NavigationStack {
-            ScrollView {
-                SearchAndFilterBar()
-                LazyVStack {
-                    ForEach(0 ... 10, id: \.self) { listing in
-                        NavigationLink(value: listing) {
-                            ListingItemView()
-                                .frame(height: 400)
-                                .clipShape(RoundedRectangle(cornerRadius: 10))
+            if showDestinationSearchView {
+                DestinationSearchView(show: $showDestinationSearchView)
+            } else {
+                ScrollView {
+                    SearchAndFilterBar()
+                        .onTapGesture {
+                            withAnimation(.snappy) {
+                                showDestinationSearchView.toggle()
+                            }
                         }
-                        
+                    LazyVStack {
+                        ForEach(0 ... 10, id: \.self) { listing in
+                            NavigationLink(value: listing) {
+                                ListingItemView()
+                                    .frame(height: 400)
+                                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                            }
+                            
+                        }
                     }
+                    .padding()
                 }
-                .padding()
+                .navigationDestination(for: Int.self) { listing in
+                    ListingDetailView()
+                        .navigationBarHidden(true)
+                       // .navigationBarBackButtonHidden()
+                }
             }
-            .navigationDestination(for: Int.self) { listing in
-                ListingDetailView()
-                    .navigationBarHidden(true)
-                   // .navigationBarBackButtonHidden()
-            }
+            
         }
         
     }
