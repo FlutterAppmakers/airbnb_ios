d899d81cedb6e28343a836df7ccec538bcaab6e3
Khawla Zarrami <zarramikhaoula@gmail.com>
2025-11-13 12:57:51 +0100

    Implement listing Map View

---
+++ AirbnbApp/Extensions/Regions.swift
@@ -0,0 +1,13 @@
+//
+//  Regions.swift
+//  AirbnbApp
+//
+//  Created by Khawla Zarrami on 13/11/2025.
+//
+
+import CoreLocation
+
+extension CLLocationCoordinate2D {
+    static var losAngeles = CLLocationCoordinate2D(latitude: 34.0549, longitude: -118.2426)
+    static var miami = CLLocationCoordinate2D(latitude: 25.7602, longitude: -80.1959)
+}
